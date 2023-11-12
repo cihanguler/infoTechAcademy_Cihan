661464e5deac54053f86c0ae00dc6b0bdbccd851
Cihan GUELER <58997250+cihanguler@users.noreply.github.com>
2023-11-12 15:25:05 +0100

    html task and JS task

---
+++ task.js
@@ -0,0 +1,13 @@
+console.log("TASK 1: 1'den 100'e kadar cift sayilari yazdiran döngüyü yaziniz.");
+
+for (let i = 2; i <= 100; i += 2) {
+    console.log(i);
+}
+
+console.log("***********************************************************");
+
+console.log("TASK 2 : Ad ve soyadinizi birlestiren JS kodu yaziniz");
+const first_name = "Cihan";
+const last_name = "Güler";
+const fullName = first_name + " " + last_name;
+console.log(fullName);
